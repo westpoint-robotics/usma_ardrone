36aeb0a72f9c1a59419c4fecd51da9254ad333c8
benjamin@neptune <abruzzo2@gmail.com>
2018-09-19 16:09:19 -0400

    I think the face tracking should work, but it will take some tuning for the appropriate gains to use

--- optitrack_controller/src/faceTrackingAutopilot.py
+++ optitrack_controller/src/faceTrackingAutopilot.py
@@ -51,8 +51,8 @@ class faceTrackingAutopilot():
         if(self.logging):
             self.switched_cmd_vel_counter += 1
             self.data_logger.write(("fta.face.switched_cmd_vel_msg.time({},1) = {:06.8f};\n").format(self.switched_cmd_vel_counter, rospy.get_time()))
-            self.data_logger.write(("fta.face.switched_cmd_vel_msg.linear({},:) = [{:06.8f} {:06.8f} {:06.8f}];\n").format(self.switched_cmd_vel_counter, self.mocap_vel_msg.linear.x, self.mocap_vel_msg.linear.y, fta.mocap_vel_msg.linear.z))
-            self.data_logger.write(("fta.face.switched_cmd_vel_msg.angular({},:) = [{:06.8f} {:06.8f} {:06.8f}];\n\n").format(self.switched_cmd_vel_counter, self.mocap_vel_msg.angular.x, self.mocap_vel_msg.angular.y, fta.mocap_vel_msg.angular.z))
+            self.data_logger.write(("fta.face.switched_cmd_vel_msg.linear({},:) = [{:06.8f} {:06.8f} {:06.8f}];\n").format(self.switched_cmd_vel_counter, self.switched_cmd_vel_msg.linear.x, self.switched_cmd_vel_msg.linear.y, fta.switched_cmd_vel_msg.linear.z))
+            self.data_logger.write(("fta.face.switched_cmd_vel_msg.angular({},:) = [{:06.8f} {:06.8f} {:06.8f}];\n\n").format(self.switched_cmd_vel_counter, self.switched_cmd_vel_msg.angular.x, self.switched_cmd_vel_msg.angular.y, fta.switched_cmd_vel_msg.angular.z))
         self.switched_cmd_vel_pub.publish(self.switched_cmd_vel_msg)
 
     def mocap_cmd_vel_cb(self,msg):
@@ -96,12 +96,15 @@ if __name__ == '__main__':
                 fta.switched_cmd_vel_msg = fta.mocap_vel_msg
         else: # let the face tracker and time switcher choose
             time_since_last_face = rospy.get_time() - fta.face_cmd_msg_time
+            # print('time_since_last_face: {}').format(time_since_last_face)
             if (time_since_last_face<=fta_face_ctrl_time):
                 # Then use face control
                 # use mocap to keep uav in center of workspace, let camera control altitude and yaw-rate
                 cmd_linear = Vector3(fta.mocap_vel_msg.linear.x, fta.mocap_vel_msg.linear.y, fta.face_cmd_msg.linear.z)
                 cmd_angular = Vector3(0, 0, fta.face_cmd_msg.angular.z)
                 fta.switched_cmd_vel_msg = Twist(cmd_linear, cmd_angular)
+                # print('cmd_linear  = [{:06.8f} {:06.8f} {:06.8f}]').format(fta.mocap_vel_msg.linear.x, fta.mocap_vel_msg.linear.y, fta.face_cmd_msg.linear.z)
+                # print('cmd_angular = [{:06.8f} {:06.8f} {:06.8f}]').format(0, 0, fta.face_cmd_msg.angular.z)
             elif(
                     time_since_last_face>fta_face_ctrl_time and 
                     time_since_last_face<=fta_wait_for_face
@@ -110,8 +113,8 @@ if __name__ == '__main__':
             else: # lost the face, go back to mocap control
                 fta.switched_cmd_vel_msg = fta.mocap_vel_msg
 
-            #publish cmd_vel no matter what
-            fta.publish_switched_cmd_vel_msg()
+        #publish cmd_vel no matter what
+        fta.publish_switched_cmd_vel_msg()
         rate.sleep()
 
     fta.data_logger.close()
